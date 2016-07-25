63f708cd0970c9a49800c2cc47f92462f8c0dec5
ulovebin <ulovewb@163.com>
2016-07-25 10:27:00 +0800

    Initial Commit

---
+++ WBViewDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBViewDrag
+//
+//  Created by tuhui－03 on 16/7/25.
+//  Copyright © 2016年 wb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBViewDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBViewDrag
+//
+//  Created by tuhui－03 on 16/7/25.
+//  Copyright © 2016年 wb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
